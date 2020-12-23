723ea70e2c666f9b67ce6c19697f75487d64283a
Zhimianwu1998 <zhimianwuchina@gmail.com>
2020-12-23 22:12:04 +0000

    Camera script

---
+++ GE_CA/Assets/Script/Camera_Control.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Control : MonoBehaviour
+{
+    public Transform Player;
+
+    private float mouseX, mouseY; // Get the value of mouse movement
+
+    public float mouseSensitivity; // Get mouse sensitivity
+
+    public float xRotation;
+
+    private void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        Player.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRotation,0,0);
+
+    }
+}
